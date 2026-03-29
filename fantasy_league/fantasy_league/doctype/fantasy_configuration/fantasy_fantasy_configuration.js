2cf76a75ad0bc09ff0f1bab69e9399e0c2c7bf09
PlayPratz <playpratz@gmail.com>
2026-03-29 14:46:29 +0530

    Removed orphaned Fantasy Configuration

--- fantasy_league/fantasy_league/doctype/fantasy_configuration/fantasy_fantasy_configuration.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Pratik Nerurkar and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Fantasy Configuration", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- fantasy_league/fantasy_league/doctype/fantasy_configuration/fantasy_fantasy_configuration.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Pratik Nerurkar and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FantasyConfiguration(Document):
-	pass

a589d378846c7e458c0055150d85303168b11d4d
PlayPratz <playpratz@gmail.com>
2026-04-16 21:24:59 +0530

    Fixed ranking issue

--- fantasy_league/utils/number_utils.py
+++ fantasy_league/utils/number_utils.py
@@ -1,8 +1,12 @@
 def rank_number_list(number_list: list, force_unique_ranks=False) -> list[int]:
 
     if force_unique_ranks:
-        sorted_number_list = sorted(number_list, reverse=True)
-        ranks = [sorted_number_list.index(num) + 1 for num in number_list]
+        sorted_indexes = [
+            i for i in sorted(enumerate(number_list), key=lambda x: x[1], reverse=True)
+        ]
+        ranks = [
+            i[0] for i in sorted(enumerate(sorted_indexes, start=1), key=lambda x: x[1])
+        ]
 
     else:
         rank_map = {}
